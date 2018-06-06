5fedc852985f0ee55106ece97dbeb1393e3b4507
Vincent Ha Vinh <vincent.havinh@hotmail.fr>
2018-06-06 18:08:57 +0200

    [Update] EditMapContract : ajout garde isReachable, ajout openDoor et closeDoor (pre post de MapContract), checkInvariant() check aussi les inv de MpaContract.

--- src/contracts/EditMapContract.java
+++ src/contracts/EditMapContract.java
@@ -19,6 +19,9 @@ public class EditMapContract extends EditMapDecorator{
 	/*========== invariants ==========*/
 	
 	public void checkInvariants() {
+		//inv dependances
+		MapContract map = new MapContract(this);
+		map.checkInvariant();
 		
 		/*Cas Map pleine de murs => pas de check isReachable, isReady doit etre faux*/
 		boolean foundNotWall = false;
@@ -39,7 +42,18 @@ public class EditMapContract extends EditMapDecorator{
 		
 		/*tous les autres cas => check isReachable sur 2 cases random != WLL, check isReady*/
 		
+		
 		//isReachable :
+		
+		/*
+		 * isReachable(M,x1,y1,x2,y2) = exists P in Array[int,int], P[0] = (x1,y1) and P[size(P)-1] = (x2,y2)
+		 * 		and forall i in [1;size(P)-1], (P[i-1]=(u,v) and P[i]=(s,t)) implies (u−s) 2 + (v−t) 2 = 1
+		 * 		and forall i in [1;size(P)-2], P[i-1]=(u,v) implies CellNature(M,u,v) != WLL
+		 * 
+		 */
+		
+		
+		//on prend deux cases aleatoires et on verifie isReachable entre elles
 		Random rand = new Random();
 		int randxi=0,randyi=0,randxo=0,randyo=0;
 		Cell c = Cell.WLL;
@@ -55,20 +69,14 @@ public class EditMapContract extends EditMapDecorator{
 			c = getCellNature(randxo, randyo);
 		}while(c == Cell.WLL);
 		
-
-		/*
-		 * isReachable(M,x1,y1,x2,y2) = exists P in Array[int,int], P[0] = (x1,y1) and P[size(P)-1] = (x2,y2)
-		 * 		and forall i in [1;size(P)-1], (P[i-1]=(u,v) and P[i]=(s,t)) implies (u−s) 2 + (v−t) 2 = 1
-		 * 		and forall i in [1;size(P)-2], P[i-1]=(u,v) implies CellNature(M,u,v) != WLL
-		 * 
-		 */
+		Pathfinder pf = new Pathfinder(this, randxi, randyi, randxo, randyo);
+		
+		
+		//garde implications isReachable
 		if(isReachable(randxi, randyi, randxo, randyo)) {
-			Pathfinder pf = new Pathfinder(this, randxi, randyi, randxo, randyo);
 			
-
 			if(pf.hasPath() == false)
 				throw new InvariantError("isReachable == true but no path existing");
-			
 			
 			for(int i = 1; i<pf.getPath().size() ; i++) {
 				int xb = pf.getPath().get(i).x;
@@ -79,12 +87,13 @@ public class EditMapContract extends EditMapDecorator{
 				if( Math.pow(xa-xb,2)+(Math.pow(yb-ya,2))!=1 ){
 				}
 				
-				if( getCellNature(xa, ya)==Cell.WLL ) {
+				if( getCellNature(xa, ya)==Cell.WLL )
 					throw new InvariantError("isReachable == true but one cell on the path is a wall");
-				}
-
 			}
 
+		}else {
+			if(pf.hasPath() == true)
+				throw new InvariantError("isReachable == false but path does exist.");
 		}
 
 
@@ -234,6 +243,66 @@ public class EditMapContract extends EditMapDecorator{
 	
 	/*========== operateurs ==========*/
 
+	public void openDoor(int x, int y) {
+		//pre
+		MapContract map = new MapContract(this);
+		map.preOpenDoor(x, y);
+		
+		//inv pre
+		checkInvariants();
+		
+		//capture
+		Cell cell_atpre = getCellNature(x, y);
+		
+		Cell cellA_atpre=null; 
+		Cell cellB_atpre=null;
+		Cell cellC_atpre=null;
+		Cell cellD_atpre=null;
+		if(x-1 >= 0) cellA_atpre = getCellNature(x-1, y);
+		if(x+1 < getWidth()) cellB_atpre = getCellNature(x+1, y);
+		if(y-1 >= 0) cellC_atpre = getCellNature(x, y-1);
+		if(y+1 < getHeight()) cellD_atpre = getCellNature(x, y+1);
+		
+		//run
+		super.openDoor(x, y);
+		
+		//inv post
+		checkInvariants();
+		
+		//post
+		map.postOpenDoor(x, y, cell_atpre, cellA_atpre, cellB_atpre, cellC_atpre, cellD_atpre);
+	}
+	
+	public void closeDoor(int x, int y) {
+		//pre
+		MapContract map = new MapContract(this);
+		map.preCloseDoor(x, y);
+		
+		//inv pre
+		checkInvariants();
+		
+		//capture
+		Cell cell_atpre = getCellNature(x, y);
+		
+		Cell cellA_atpre=null; 
+		Cell cellB_atpre=null;
+		Cell cellC_atpre=null;
+		Cell cellD_atpre=null;
+		if(x-1 >= 0) cellA_atpre = getCellNature(x-1, y);
+		if(x+1 < getWidth()) cellB_atpre = getCellNature(x+1, y);
+		if(y-1 >= 0) cellC_atpre = getCellNature(x, y-1);
+		if(y+1 < getHeight()) cellD_atpre = getCellNature(x, y+1);
+		
+		//run
+		super.closeDoor(x, y);
+		
+		//inv post
+		checkInvariants();
+		
+		//post
+		map.postCloseDoor(x, y, cell_atpre, cellA_atpre, cellB_atpre, cellC_atpre, cellD_atpre);
+	}
+	
 	public EditMapService setNature(int x, int y, Cell c) {
 		//pre
 		preSetNature(x, y);
